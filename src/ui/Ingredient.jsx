f474694d7ce15f293bb4fbb4d6c0f0d90ae387eb
Paolo Galli <macbookpro@Mac.station>
2024-11-18 19:12:59 +0100

    added formatIngredient to helpers

--- src/ui/Ingredient.jsx
+++ src/ui/Ingredient.jsx
@@ -1,22 +1,11 @@
-import Fraction from "fraction.js";
+import { formatIngredient } from "../utils/helpers";
 
 function Ingredient({ ingredients, servings, numOfGuests }) {
   const { id, amount, unit, name } = ingredients;
   return (
     <li key={id}>
       {amount
-        ? (() => {
-            const totalAmount = (amount / servings) * numOfGuests;
-            const integerPart = Math.trunc(totalAmount);
-            const decimalPart = totalAmount - integerPart;
-
-            const fractionPart =
-              decimalPart > 0 ? new Fraction(decimalPart).toFraction(true) : "";
-
-            return `${integerPart > 0 ? integerPart : ""} ${fractionPart} ${
-              unit ? `${unit} of` : ""
-            } ${name}`;
-          })()
+        ? formatIngredient(amount, servings, numOfGuests, unit, name)
         : name}
     </li>
   );
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,25 @@
+import Fraction from "fraction.js";
 import noImg from "../assets/img/no_img.jpg";
 
 export const handleImgError = function (e) {
   e.target.src = noImg;
+};
+
+export const formatIngredient = function (
+  amount,
+  servings,
+  numOfGuests,
+  unit,
+  name,
+) {
+  const totalAmount = (amount / servings) * numOfGuests;
+  const integerPart = Math.trunc(totalAmount);
+  const decimalPart = totalAmount - integerPart;
+
+  const fractionPart =
+    decimalPart > 0 ? new Fraction(decimalPart).toFraction(true) : "";
+
+  return `${integerPart > 0 ? integerPart : ""} ${fractionPart} ${
+    unit ? `${unit} of` : ""
+  } ${name}`;
 };
